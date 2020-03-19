f5c69a99d81e7e22ca4f1fe8e0764cec96143059
Mickey <dunyazad@hanmail.net>
2020-03-19 09:05:07 +0900

    Add Express Web Server
    Add Express Web Server

---
+++ public/js/Feather.js
@@ -0,0 +1,10 @@
+class Feather {
+    constructor(htmlElement) {
+        this.htmlElement = htmlElement;
+        this.gl = this.htmlElement.getContext('experimental-webgl');
+    }
+}
+
+class Buffer {
+    
+}
---
+++ public/js/index.js
@@ -0,0 +1,3 @@
+document.onload = function () {
+    var feather = new Feather(document.getElementById("canvas"));
+}
