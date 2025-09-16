8670503abdd9bf44cf570c3483f7a86a7994dbc2
Aaron <aaron@huvitz.com>
2025-09-16 11:41:07 +0900

    Toggle ImmediateModeRenderSystem

--- include/System/ImmediateModeRenderSystem.h
+++ include/System/ImmediateModeRenderSystem.h
@@ -14,6 +14,12 @@ public:
 	virtual void Terminate();
 	virtual void Update(ui32 frameNo, f32 timeDelta);
 
+	inline bool IsEnabled() const { return bEnabled; }
+	inline void SetEnable(bool enable) { bEnabled = enable; }
+	inline void ToggleEnable() { bEnabled = !bEnabled; }
+
 private:
 	FeatherWindow* window = nullptr;
+
+	bool bEnabled = true;
 };
